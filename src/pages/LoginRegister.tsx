76a4b72b561d92c0f6c7320b178082d916acfbb9
unknown <jeffry0414@gmail.com>
2025-10-23 15:46:06 -0500

    feat: added css to login

--- src/pages/LoginRegister.tsx
+++ src/pages/LoginRegister.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import "../styles/login/LoginRegister.css";
 
 
 function LoginRegister(){
@@ -25,32 +26,32 @@ function LoginRegister(){
     const [numero, setNumero] = useState("");
     return(
     <div className="logContainer">
-        <div className="login" style={{border:"1px solid black", padding: "20px"}}>
+        <div className="login containers">
             <h2>Login</h2>
 
             <h5>Email</h5>
-            <form action="" onSubmit={handleLogin}>
-                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <form  action="" onSubmit={handleLogin}>
+                <input className="inputLog" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <h5>Password</h5>
-                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input className="inputLog" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit">Login</button>
             </form>
         </div>
-        <div className="register" style={{border:"1px solid black", padding: "20px"}}>
+        <div className="register containers" >
             <h2>Register</h2>
             
             <h5>Nombres</h5>
-            <input type="text" placeholder="Nombres" value={nombres} onChange={(e) => setNombres(e.target.value)} />
+            <input className="inputLog" type="text" placeholder="Nombres" value={nombres} onChange={(e) => setNombres(e.target.value)} />
             <h5>Apellidos</h5>
-            <input type="text" placeholder="Apellidos" value={apellidos} onChange={(e) => setAppellidos(e.target.value)} />
+            <input className="inputLog" type="text" placeholder="Apellidos" value={apellidos} onChange={(e) => setAppellidos(e.target.value)} />
             <h5>Edad</h5>
-            <input type="number" placeholder="Edad" value={age} onChange={(e) => setAge(e.target.value)} />
+            <input className="inputLog" type="number" placeholder="Edad" value={age} onChange={(e) => setAge(e.target.value)} />
             <h5>Numero</h5>
-            <input type="number" placeholder="Numero" value={numero} onChange={(e) => setNumero(e.target.value)} />
+            <input className="inputLog" type="number" placeholder="Numero" value={numero} onChange={(e) => setNumero(e.target.value)} />
             <h5>Email</h5>
-            <input type="email" placeholder="Email" value={emailR} onChange={(e) => setEmailR(e.target.value)} />
+            <input className="inputLog" type="email" placeholder="Email" value={emailR} onChange={(e) => setEmailR(e.target.value)} />
             <h5>Password</h5>
-            <input type="password" placeholder="Password" value={passwordR} onChange={(e) => setPasswordR(e.target.value)} />
+            <input className="inputLog" type="password" placeholder="Password" value={passwordR} onChange={(e) => setPasswordR(e.target.value)} />
 
         </div>
     </div>
