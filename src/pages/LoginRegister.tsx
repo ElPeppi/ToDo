8a83925c58bb88b53d99564ad92e1041d9e9dd2e
unknown <jeffry0414@gmail.com>
2025-10-24 18:00:29 -0500

    feat:update login funtional

--- src/pages/LoginRegister.tsx
+++ src/pages/LoginRegister.tsx
@@ -3,58 +3,71 @@ import { useState } from "react";
 import "../styles/login/LoginRegister.css";
 
 
-function LoginRegister(){
+function LoginRegister() {
     const navigate = useNavigate();
 
-    const handleLogin = (e: React.FormEvent<HTMLFormElement>)=>{
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:4000/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
+            });
 
-        if(email === "admin@example.com" && password === "password") {
-            navigate("/ToDo");
-        } else {
-            alert("Invalid credentials");
+            const data = await response.json();
+            console.log(data);
+
+            // ejemplo: redirigir si el login fue exitoso
+            if (response.ok) {
+                localStorage.setItem("token", data.token);
+                
+                navigate("/ToDo");
+                console.log("Inicio de sesión exitoso:", data);
+            } else {
+                console.error("Error en login:", data.message);
+            }
+        } catch (error) {
+            console.error("Error al hacer login:", error);
         }
-    }
+    };
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordR, setPasswordR] = useState("");
     const [emailR, setEmailR] = useState("");
-    const [nombres, setNombres] = useState("");
-    const [apellidos, setAppellidos] = useState("");
-    const [age, setAge] = useState("");
-    const [numero, setNumero] = useState("");
-    return(
-    <div className="logContainer">
-        <div className="login containers">
-            <h2>Login</h2>
+    const [fullName, setfullName] = useState("");
 
-            <h5>Email</h5>
-            <form  action="" onSubmit={handleLogin}>
-                <input className="inputLog" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+    return (
+        <div className="logContainer">
+            <div className="login containers">
+                <h2>Login</h2>
+
+                <h5>Email</h5>
+                <form action="" onSubmit={handleLogin}>
+                    <input className="inputLog" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <h5>Password</h5>
+                    <input className="inputLog" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+            <div className="register containers" >
+                <h2>Register</h2>
+
+                <h5>Nombre Completo</h5>
+                <input className="inputLog" type="text" placeholder="Nombre Completo" value={fullName} onChange={(e) => setfullName(e.target.value)} />
+                <h5>Email</h5>
+                <input className="inputLog" type="email" placeholder="Email" value={emailR} onChange={(e) => setEmailR(e.target.value)} />
                 <h5>Password</h5>
-                <input className="inputLog" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-        <div className="register containers" >
-            <h2>Register</h2>
-            
-            <h5>Nombres</h5>
-            <input className="inputLog" type="text" placeholder="Nombres" value={nombres} onChange={(e) => setNombres(e.target.value)} />
-            <h5>Apellidos</h5>
-            <input className="inputLog" type="text" placeholder="Apellidos" value={apellidos} onChange={(e) => setAppellidos(e.target.value)} />
-            <h5>Edad</h5>
-            <input className="inputLog" type="number" placeholder="Edad" value={age} onChange={(e) => setAge(e.target.value)} />
-            <h5>Numero</h5>
-            <input className="inputLog" type="number" placeholder="Numero" value={numero} onChange={(e) => setNumero(e.target.value)} />
-            <h5>Email</h5>
-            <input className="inputLog" type="email" placeholder="Email" value={emailR} onChange={(e) => setEmailR(e.target.value)} />
-            <h5>Password</h5>
-            <input className="inputLog" type="password" placeholder="Password" value={passwordR} onChange={(e) => setPasswordR(e.target.value)} />
+                <input className="inputLog" type="password" placeholder="Password" value={passwordR} onChange={(e) => setPasswordR(e.target.value)} />
 
+            </div>
         </div>
-    </div>
     )
 }
 
